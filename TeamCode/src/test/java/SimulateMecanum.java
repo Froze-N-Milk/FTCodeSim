d4a44e79a68748dc8d1ea97324cd71f940a522ec
AchintyaAkula <achintya.akula@gmail.com>
2026-07-04 12:03:00 -0700

    asdf

--- TeamCode/src/test/java/SimulateMecanum.java
+++ TeamCode/src/test/java/SimulateMecanum.java
@@ -18,13 +18,13 @@ public class SimulateMecanum {
         mecanumConfig.frontRightMotorName = "frontRight";
         mecanumConfig.backLeftMotorName = "backLeft";
         mecanumConfig.backRightMotorName = "backRight";
-        mecanumConfig.wheelbase = 4.68504 * 2; // distance from center of frontLeft wheel to backLeft wheel
-        mecanumConfig.trackWidth = 4.56693 * 2; // distance from center of backRight wheel to backLeft wheel
+        mecanumConfig.wheelbase = 9.5767; // distance from center of frontLeft wheel to backLeft wheel
+        mecanumConfig.trackWidth = 8.66142; // distance from center of backRight wheel to backLeft wheel
         mecanumConfig.wheelRadius = 3.77953 / 2;
         mecanumConfig.staticVelocityRegion = 2;
         mecanumConfig.staticFriction = 45;
         mecanumConfig.maxAcceleration = 250;
-        mecanumConfig.maxVelocity = 70;
+        mecanumConfig.maxVelocity = 89.272;
         mecanumConfig.naturalDeceleration = 40;
         mecanumConfig.strafeEfficiency = 0.90;
         mecanumConfig.fakeHardwareMap = fakeHardwareMap;
